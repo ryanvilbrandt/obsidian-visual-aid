3099b8718b5b069165f4c1c147c35f731a8c5d37
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2023-03-04 10:37:57 -0800

    Use svg for icons

--- main.js
+++ main.js
@@ -111,14 +111,29 @@ function create_audio_controls(settings) {
     const controls_div = document.createElement("div");
     controls_div.id = "audio-controls-div";
     const actions = [
-        {"action": "play", "label": "▶"},
-        {"action": "pause", "label": "⏸"},
-        {"action": "stop", "label": "⏹"},
+        {
+            "action": "play",
+            "label": "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-6 h-6\">\n" +
+                "  <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z\" />\n" +
+                "</svg>"
+        },
+        {
+            "action": "pause",
+            "label": "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-6 h-6\">\n" +
+                "  <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M15.75 5.25v13.5m-7.5-13.5v13.5\" />\n" +
+                "</svg>"
+        },
+        {
+            "action": "stop",
+            "label": "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke-width=\"1.5\" stroke=\"currentColor\" class=\"w-6 h-6\">\n" +
+                "  <path stroke-linecap=\"round\" stroke-linejoin=\"round\" d=\"M5.25 7.5A2.25 2.25 0 017.5 5.25h9a2.25 2.25 0 012.25 2.25v9a2.25 2.25 0 01-2.25 2.25h-9a2.25 2.25 0 01-2.25-2.25v-9z\" />\n" +
+                "</svg>"
+        },
     ];
     for (const i in actions) {
         const control_div = document.createElement("div");
         const audio_link = document.createElement("a");
-        audio_link.innerText = toTitleCase(actions[i]["label"]);
+        audio_link.innerHTML = actions[i]["label"];
         audio_link.href = `${actions[i]["action"]}|all`;
         audio_link.onclick = (event) => set_audio_file(event, settings);
         control_div.appendChild(audio_link);
